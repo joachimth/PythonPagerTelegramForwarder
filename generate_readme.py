3bf1f6064512f215b00fe158abe045c74b651133
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2023-08-10 12:05:59 +0200

    Create generate_readme.py

---
+++ generate_readme.py
@@ -0,0 +1,17 @@
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+# Load data from YAML file
+with open("appdefs.yml", 'r') as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+
+# Configure Jinja and ready the template
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('readme_template.md')
+
+# Process the template to produce our final text.
+output_text = template.render(cfg)
+
+# Write to README.md
+with open("README.md", "w") as fh:
+    fh.write(output_text)
