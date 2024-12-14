f68388c3455403f3c9efb7e0d5bd6e4f33d4d9d0
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-12-14 11:34:14 +0100

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify  # Tilføj jsonify her
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 import configparser
 import os
