9898e01cbb1ce4121f30f4e418cbfc8fd4afeff8
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-12-17 21:45:59 +0100

    Update initialize_database.py

--- initialize_database.py
+++ initialize_database.py
@@ -12,31 +12,13 @@ def initialize_database(db_path='messages.db'):
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
         raw_message TEXT NOT NULL,
-        stednavn TEXT,
-        adresse TEXT,
-        postnr TEXT,
-        by TEXT,
-        lat TEXT,
-        long TEXT,
-        linktilgooglemaps TEXT,
-        alarmtype_kritisk BOOLEAN,
-        alarmtype_seriøs BOOLEAN,
-        alarmtype_lavrisiko BOOLEAN,
-        alarmtype_højrisiko BOOLEAN,
-        alarmkald_politi BOOLEAN,
-        alarmkald_brand BOOLEAN,
-        alarmkald_isl BOOLEAN,
-        alarmkald_medicin BOOLEAN,
-        alarmkald_vagt BOOLEAN,
-        alarmkald_specialrespons BOOLEAN,
-        specifik_brandalarm BOOLEAN,
-        specifik_gasledningsbrud BOOLEAN,
-        specifik_bygningsbrand BOOLEAN
+        parsed_fields TEXT DEFAULT NULL
     )
     """)
     
     conn.commit()
     conn.close()
+    print(f"Database '{db_path}' initialiseret og klar til brug.")
 
 if __name__ == "__main__":
     initialize_database()
