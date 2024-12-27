96dadc5875db6496896373b0cd848d58b731fdef
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2024-12-27 23:52:03 +0100

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -21,7 +21,10 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 
 # Dummy brugerdata
-users = {"joachimth@nowhere.com": {"password": "changeme"}}
+#users = {"joachimth@nowhere.com": {"password": "changeme"}}
+
+# Dummy brugerdata
+users = {'joachimth@nowhere.com': {'password': 'changeme'}}
 
 class User(UserMixin):
     pass
