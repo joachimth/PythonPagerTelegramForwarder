4c8db44039604910b4195f342a47954dc48dd059
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2025-01-02 22:43:41 +0100

    Update script_runner.py

--- script_runner.py
+++ script_runner.py
@@ -6,11 +6,11 @@ def run_script(script_name, port=None):
     if port:
         command.append(str(port))
 
-    logging.info(f"Kører script: {' '.join(command)}")
+    #logging.info(f"Kører script: {' '.join(command)}")
 
     try:
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        logging.info(f"Output fra {script_name}:\n{result.stdout}")
+        #logging.info(f"Output fra {script_name}:\n{result.stdout}")
         if result.returncode != 0:
             logging.error(f"Fejl under kørsel af {script_name}: {result.stderr}")
             return False
